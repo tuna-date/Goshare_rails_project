a0eafda14b569c21f6d30e5ece740aaa27d002a1
hoang <vinhyenvodoi98@gmail.com>
2019-11-24 00:49:40 +0700

    feat : login

--- frontend/src/views/Login/index.js
+++ frontend/src/views/Login/index.js
@@ -4,23 +4,56 @@ import './Login.css';
 class Login extends React.Component {
   render() {
     return (
-      <article className='Post' ref='Post'>
-        <header>
-          <div className='Post-user'>
-            <div className='Post-user-avatar'>hellp</div>
-            <div className='Post-user-nickname'>
-              <span>hoelpp</span>
+      <div className='device'>
+        <div className='row container'>
+          <div className='col-8'>
+            <div className='device-size'>
+              <figure className='iphone'>iPhone</figure>
+              <figure className='ipad'>iPad</figure>
             </div>
           </div>
-        </header>
-        <div className='Post-image'>
-          <div className='Post-image-bg'>he;;p</div>
+
+          <div className='col-4 box'>
+            <p className='title'>Login to Goshare</p>
+            <div className='col'>
+              <button className='login-div button-size loginBtn loginBtn--facebook'>
+                Login with Facebook
+              </button>
+
+              <button className='login-div button-size loginBtn loginBtn--google'>
+                Login with Google
+              </button>
+            </div>
+            <div className='separator'>OR</div>
+            <div className='form-group login-div'>
+              <div className='col button-size'>
+                <input
+                  type='email'
+                  className='form-control'
+                  id='exampleInputEmail1'
+                  aria-describedby='emailHelp'
+                  placeholder='Enter email'
+                />
+              </div>
+            </div>
+            <div className='form-group login-div'>
+              <div className='col button-size'>
+                <input
+                  type='password'
+                  className='form-control'
+                  id='exampleInputPassword1'
+                  placeholder='Password'
+                />
+              </div>
+            </div>
+            <div className='col login-div'>
+              <button type='submit' className='btn btn-primary button-size'>
+                Login
+              </button>
+            </div>
+          </div>
         </div>
-        <div className='Post-caption'>
-          <strong>hoelpp</strong>
-          sdds
-        </div>
-      </article>
+      </div>
     );
   }
 }
