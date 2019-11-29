c801e3aaad2a5b7aecb4250c6fa5e4d136005908
hoang <vinhyenvodoi98@gmail.com>
2019-11-30 00:56:45 +0700

    feat : auto login and post images to firebase

---
+++ frontend/src/firebase/index.example.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/storage';
+
+// Initialize Firebase
+var config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
+};
+
+firebase.initializeApp(config);
+
+const storage = firebase.storage();
+
+export { storage, firebase as default };
