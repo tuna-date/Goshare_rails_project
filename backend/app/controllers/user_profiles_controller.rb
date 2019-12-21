d776ba02751b18d0b4a147d56723599a4c3bf7b9
anhtuhsp <anhtuhspk61@gmail.com>
2019-12-21 16:44:13 +0700

    show all post in user profile

--- backend/app/controllers/user_profiles_controller.rb
+++ backend/app/controllers/user_profiles_controller.rb
@@ -10,25 +10,21 @@ class UserProfilesController < ApplicationController
     # Show info of a user profile
     def show
         user_profile = {}
-        user_profile["id"] = @user.id
-        user_profile["name"] = @user.name
-        user_profile["email"] = @user.email
-        user_profile["avatar_url"] = @user.avatar_url
-        user_profile["posts_count"] = @user.posts.count
-        user_profile["following_count"] = @user.following.count
-        user_profile["followers_count"] = @user.followers.count
-        user_profile["is_following_by_current_user"] = @current_user.following.include?(@user)
-        user_profile["posts"] = []
-        sql = " SELECT * 
-                FROM   posts
-                WHERE  user_profile_id = '#{@user.id}'
-                ORDER BY created_at DESC"
-        posts = Post.paginate_by_sql(sql, page: params[:page], per_page: 5)
+        user_profile[:id] = @user.id
+        user_profile[:name] = @user.name
+        user_profile[:email] = @user.email
+        user_profile[:avatar_url] = @user.avatar_url
+        user_profile[:posts_count] = @user.posts.count
+        user_profile[:following_count] = @user.following.count
+        user_profile[:followers_count] = @user.followers.count
+        user_profile[:is_following_by_current_user] = @current_user.following.include?(@user)
+        user_profile[:posts] = []
+        posts =  @user.posts
         posts.each do |post|
             profile_post = {}
             profile_post[:post_id] = post.id
             profile_post[:image_url] = post.image_url
-            user_profile["posts"].push(profile_post)
+            user_profile[:posts].push(profile_post)
         end
         render json: user_profile, status: :ok
     end
