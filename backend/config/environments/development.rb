99545d4c96f7e471bd2d4df38a5ecda176826701
hoang <vinhyenvodoi98@gmail.com>
2019-12-21 22:39:52 +0700

    feat: complete realtime notification

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -27,6 +27,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_cable.mount_path = '/cable'
+
+  config.action_cable.url = 'ws://localhost:5050/cable'
+
+  config.action_cable.allowed_request_origins = [ /http:\/\/localhost:*/ ]
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
